47c091aa6e66d23e828beb6eb5d65e6265ed3d34
psyhl7 <psyhl7@exmail.nottingham.ac.uk>
2025-05-10 18:35:53 +0100

    refactor: Type updated

--- properties.js
+++ properties.js
@@ -2,7 +2,7 @@
  * Name as used as title for the main page
  * @type {string}
  */
-var title = 'COMP4037 SurVis Literature Collection';
+var title = 'SurVis Literature Collection';
 
 /**
  * Relative path to the data directory
@@ -54,7 +54,7 @@ var editable = true;
  * @type {{html: string, id: string}}
  */
 var paper = {
-    html: 'Literature collection of Ho Fung Leung, Student ID: 20378468',
+    html: 'Sample literature collection for <b><a href="http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/eurovis14-star.pdf" target="_blank">SurVis</a></b>',
     id: 'Beck2016Visual'
 };
 
